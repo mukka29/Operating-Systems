aee1bcb8fc1d9d2612d80771aa937b948b1b30df
Bharath Muka <mukkadev@gmail.com>
2020-11-05 13:48:38 -0600

    Update data.h

--- Assignment3_version2/data.h
+++ Assignment3_version2/data.h
@@ -1,27 +1,27 @@
 #include <time.h>
 
-//Constants for our program
+//Constants used for this program
 #define PATH_KEY "/"
 #define MSG_KEY 1111
 #define MEM_KEY 2222
 
 struct data {
-	struct timeval timer;	//time
-	int user_pid;	//user PID that is exiting
+	struct timeval timer;	// represents the time
+	int user_pid;	//user PID which is exiting
 };
 
 enum queue_msg {
-	MSG_ALL=0,	//accept message from everybody
-	MSG_ENTER,	//when a process wants to enter critical section
-	MSG_LEAVE,	//when a process wants to leave critical section
-	MSG_EXIT		//when a process wants to terminate
+	MSG_ALL=0,	//accepting message from everybody i.e., message all
+	MSG_ENTER,	//used when a process wants to enter critical section i.e., message enter
+	MSG_LEAVE,	//when a process wants to leave critical section i.e., message leave
+	MSG_EXIT	//when a process wants to terminate i.e., message exit
 };
 
-//Message queue buffer
+//Buffer for message queue
 struct msgbuf {
 	long mtype;	//the type of message
 	int pid;		//who is sending the message. Needed by master, to send reply
 };
 
-//how big is our message ( without the mtype)
+//shows how big is the message, i.e., our message buffer size (without the mtype)
 #define MSGBUF_SIZE sizeof(int)
