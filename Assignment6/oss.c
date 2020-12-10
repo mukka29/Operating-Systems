b44ca091e737fb886785152a54580d58a1b0c064
Bharath Muka <mukkadev@gmail.com>
2020-12-10 01:51:40 -0600

    Updated comments for more readability

--- Assignment6/oss.c
+++ Assignment6/oss.c
@@ -1,8 +1,8 @@
+//header files
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <errno.h>
 #include <stdio.h>
 #include <sys/shm.h>
 #include <sys/msg.h>
@@ -12,12 +12,15 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/time.h>
+#include <errno.h>
 
 #include "queue.h"
 
+/*  Most of the code for this project is from the previous projects per this repository */
+
 struct option{
-  int val;  //current value
-  int max;  //maximum value
+  int val;  //the current value
+  int max;  //and the maximum value
 };
 
 struct mstat{
@@ -32,7 +35,7 @@ struct mstat{
 } mstat;
 
 struct option started, running, runtime, addr_scheme;  //-n, -s, -t, -m options
-struct option exited; //exited processes and word index
+struct option exited; //for the processes which are exited and for word index
 static int verbose = 0;
 
 static int mid=-1, qid = -1, sid = -1;
@@ -41,16 +44,17 @@ static struct data *data = NULL;
 static int loop_flag = 1;
 static struct timeval timestep;
 
-static queue_t blocked; //blocked queue
+static queue_t blocked; //for the blocked queue
 
-static unsigned int bitvector = 0; //bit vector for user[]
+//represents the bit vector for user[]
+static unsigned int bitvector = 0; 
 static unsigned char frame_bitvector[(FTBL_SIZE / 8) + 1];
 
 static void switch_bit(const int b){
   bitvector ^= (1 << b);
 }
 
-//Find a frame to be evicted, based on LRU
+//Finding a frame to be evicted, based on Least Recently used (LRU)
 static int eviction_policy(struct frame * frame_tbl){
   int i, evicted = 0;
   for(i=0; i < FTBL_SIZE; i++){
@@ -76,7 +80,7 @@ static void frame_mark_used(struct frame *frame, const int f){
   frame->state = FR_USED;
 }
 
-//Find unused frame
+//Finding the unused frame
 static int find_frame(){
 	int i;
   for(i=0; i < FTBL_SIZE; i++){
@@ -130,12 +134,12 @@ static void mem_alloc(){
 
   bzero(frame_bitvector, sizeof(frame_bitvector));
 
-  //initialize the page and frame tables
+  //initializing the page and the frame tables
 	for(i=0; i < RUNNING_MAX; i++){
     ptbl_reset(data->users[i].PT, data->FT);
   }
 
-	//clear frame table
+	//clearing the frame table
 	for(i=0; i < FTBL_SIZE; i++){
 		frame_reset(&data->FT[i], i);
   }
@@ -161,7 +165,7 @@ static int critical_lock(){
 	return 0;
 }
 
-//Unlock critical section
+//Unlocking the critical section
 static int critical_unlock(){
 	struct sembuf sop;
 	sop.sem_num = 0;
@@ -174,12 +178,12 @@ static int critical_unlock(){
 	return 0;
 }
 
-/* Return bit value */
+// Returning bit value
 static int check_bit(const int b){
   return ((bitvector & (1 << b)) >> b);
 }
 
-/* Find unset bit */
+// Finding the unset bit
 static int find_bit(){
   int i;
   for(i=0; i < RUNNING_MAX; i++){
@@ -190,7 +194,7 @@ static int find_bit(){
   return -1;
 }
 
-/* Increment a timer */
+// Incrementing the timer
 static void timerinc(struct timeval *a, struct timeval * inc){
   struct timeval res;
   critical_lock();
@@ -236,12 +240,12 @@ static void deny_all(){
   sigset_t mask, oldmask;
   struct msgbuf mbuf;
 
-  //tell all to stop
+  //Informing all to stop
   sigemptyset(&mask);
   sigaddset(&mask, SIGCHLD);
   sigprocmask(SIG_BLOCK, &mask, &oldmask);
 
-  //deny all blocked reference requests
+  //Denying all the blocked reference requests
   for(i=0; i < RUNNING_MAX; i++){
     if(data->users[i].pid == 0){
       continue;
@@ -264,19 +268,18 @@ static void deny_all(){
   sigprocmask(SIG_SETMASK, &oldmask, NULL);
 }
 
-//Exit master cleanly
+//Exiting master cleanly
 static void clean_exit(const int code){
 
   if(data){
 
-    //
     critical_lock();
     data->stop = 1;
     critical_unlock();
 
-    //send empty slice to all running processes to terminate
+//sending empty slice to all running processes for termination
     while(running.val > 0){
-      //some user may be waiting on timer
+//some user may be waiting on timer
       data->timer.tv_sec++;
       deny_all();
       reap_zombies();
@@ -288,7 +291,7 @@ static void clean_exit(const int code){
 
   q_deinit(&blocked);
 
-  //clean shared memory and semaphores
+  //cleaning shared memory and the semaphores
   shmctl(mid, IPC_RMID, NULL);
   msgctl(qid, 0, IPC_RMID);
   semctl(sid, 0, IPC_RMID);
@@ -335,7 +338,7 @@ static int spawn_user(){
 	return pid;
 }
 
-//Show usage menu
+//this shows the usage menu in this project
 static void usage(){
   printf("Usage: master [-v] [-c 5] [-l log.txt] [-t 20]\n");
   printf("\t-h\t Show this message\n");
@@ -348,7 +351,7 @@ static void usage(){
 //Set options specified as program arguments
 static int set_options(const int argc, char * const argv[]){
 
-  //default options
+  //these are the default options used as in the previous Projects
   started.val = 0; started.max = 100; //max users started
   exited.val  = 0; exited.max = 100;
   runtime.val = 0; runtime.max = 2;  //maximum runtime in real seconds
@@ -385,7 +388,7 @@ static int set_options(const int argc, char * const argv[]){
   return 0;
 }
 
-//Attach the data in shared memory
+//Attaching the data in shared memory
 static int attach_data(){
 
 	key_t k1 = ftok(PATH_KEY, MEM_KEY);
@@ -396,7 +399,7 @@ static int attach_data(){
 		return -1;
 	}
 
-  //create the shared memory area
+  //creating the shared memory area
   const int flags = IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR;
 	mid = shmget(k1, sizeof(struct data), flags);
   if(mid == -1){
@@ -404,7 +407,7 @@ static int attach_data(){
   	return -1;
   }
 
-  //create the message queue
+  //creating the message queue
 	qid = msgget(k2, flags);
   if(qid == -1){
   	perror("msgget");
@@ -417,17 +420,17 @@ static int attach_data(){
   	return -1;
   }
 
-  //attach to the shared memory area
+  //attaching to the shared memory area
 	data = (struct data *) shmat(mid, NULL, 0);
 	if(data == NULL){
 		perror("shmat");
 		return -1;
 	}
 
-  //clear the memory
+  //clearing the memory
   bzero(data, sizeof(struct data));
 
-  //set semaphores
+  //setting semaphores
   unsigned short val = 1;
 	if(semctl(sid, 0, SETVAL, val) ==-1){
 		perror("semctl");
@@ -441,18 +444,18 @@ static int attach_data(){
 static void sig_handler(const int signal){
 
   switch(signal){
-    case SIGINT:  //interrupt signal
+    case SIGINT:  //for interrupt signal
       printf("Master: Signal INT receivedat system time %lu:%li\n", data->timer.tv_sec, data->timer.tv_usec);
       loop_flag = 0;  //stop master loop
       break;
 
-    case SIGALRM: //alarm - end of runtime
+    case SIGALRM: //creates an alarm - for end of runtime
       printf("Master:  Signal ALRM received at system time %lu:%li\n", data->timer.tv_sec, data->timer.tv_usec);
       loop_flag = 0;
       data->stop = 1;
       break;
 
-    case SIGCHLD: //user program exited
+    case SIGCHLD: //for user program exited
       reap_zombies();
       break;
 
@@ -479,7 +482,7 @@ static int schedule_blocked(){
       struct virt_page * vp = &usr->PT[pidx];
 
       eviction_update_timestamp(data->FT, vp->f);
-
+//when process becomes ready
       usr->state = ST_READY;
       usr->ref.state = REF_ACCEPT;  //change request state to precess
 
@@ -487,7 +490,7 @@ static int schedule_blocked(){
       printf("OSS: Unblocked process with PID %d waiting on addres %d at system time %lu:%li\n",
         usr->id, usr->ref.vaddr, data->timer.tv_sec, data->timer.tv_usec);
 
-      //unblock the user
+      //unblocking the user
       mbuf.mtype = usr->pid;  //set its pid as type
       mbuf.user_id = usr->id;
       if(msgsnd(qid, (void*)&mbuf, MSGBUF_SIZE, 0) == -1){
@@ -504,28 +507,28 @@ static int mem_addr_fault(struct user_pcb * usr){
 
 	mstat.num_fault++;
 
-	//get page index
+	//to get the page index
   const int pidx = PAGE_IDX(usr->ref.vaddr);
 
-	//get a free frame index
+	//to get a free frame index
   int f = find_frame();
   if(f >= 0){
 		printf("Master: Using free frame %d for P%d page %d at system time %lu:%li\n",
       f, usr->id, pidx, data->timer.tv_sec, data->timer.tv_usec);
 
-  }else{	//no frame is free
+  }else{	//when there is no frame which is free
 
 		mstat.num_evict++;
 	  f = eviction_policy(data->FT);
 
 	  struct frame * fr = &data->FT[f];
 	  struct virt_page * vp = &data->users[fr->u].PT[fr->p];
-
+	//evicting page
 	  printf("Master: Evicting page %d of P%d at system time %li:%lu\n",
 	      fr->p, fr->u, data->timer.tv_sec, data->timer.tv_usec);
 
 	  vp->ref = 0;
-
+	// clearing and swapping
 	  printf("Master: Clearing frame %d and swapping in P%d page %d at system time %li:%lu\n",
 	    vp->f, usr->id, pidx, data->timer.tv_sec, data->timer.tv_usec);
 
@@ -533,7 +536,7 @@ static int mem_addr_fault(struct user_pcb * usr){
       struct timeval tv;
       tv.tv_sec = 0; tv.tv_usec = 14;
       timerinc(&data->timer, &tv);
-
+	//Dirty Bit setting and adding additionla time to the clock
 			printf("Master: Dirty bit of frame %d is set, adding additional time to clock at system time %li:%lu\n",
 				vp->f, data->timer.tv_sec, data->timer.tv_usec);
 		}
@@ -554,27 +557,27 @@ static int mem_page_load(struct user_pcb * usr){
 
 	struct virt_page * vp = &usr->PT[pidx];
 
-	//if page has no frame (not in memory)
+	//if page has no frame (i.e., not in memory)
   if(vp->f < 0){
 
     printf("Master: Address %d is not in a frame, pagefault at system time %li:%lu\n",
       usr->ref.vaddr, data->timer.tv_sec, data->timer.tv_usec);
 
-		//save frame index in page
+		//for saving frame index in page
   	vp->f = mem_addr_fault(usr);
   	vp->ref = 1;
 
-		//mark frame as used, and save page/user details
+		//for marking frame as used, and save page/user details
     frame_mark_used(&data->FT[vp->f], vp->f);
   	data->FT[vp->f].p = pidx;
   	data->FT[vp->f].u = usr - data->users;
 
-    //14 ms load time for device
+    //14 ms load time for the device
     tv.tv_sec = 0; tv.tv_usec = 14;
     usr->ref.loadt = data->timer;
     timerinc(&usr->ref.loadt, &tv);
 
-		//add to blocked queue
+		//adding to the blocked queue
     const int user_id = usr - data->users;
     q_enq(&blocked, user_id);
 
@@ -614,7 +617,7 @@ static int schedule_dispatch(){
   //printf("OSS: Acknowledged process with PID %u is making a reference for address %d at system time %lu:%li\n",
   //  usr->id, usr->ref.vaddr, data->timer.tv_sec, data->timer.tv_usec);
 
-  //process the request
+  //processing the request
   int reply = 0;
   if(usr->ref.req == MEM_READ){ //if user wants to MEM_READ
 
@@ -630,7 +633,7 @@ static int schedule_dispatch(){
       printf("Master: Address %d in frame %d, giving data to P%d at system time %li:%lu\n", usr->ref.vaddr, vp->f, usr->id, data->timer.tv_sec, data->timer.tv_usec);
 
       reply = 1;
-    }else if(usr->ref.state == REF_DENY){
+    }else if(usr->ref.state == REF_DENY){ //reference for address is denied
       printf("Master: P%d refrence for address %d was denied %lu:%lu\n", usr->id, usr->ref.vaddr, data->timer.tv_sec, data->timer.tv_usec);
       reply = 1;
     }
@@ -650,7 +653,7 @@ static int schedule_dispatch(){
         usr->ref.vaddr, vp->f, data->timer.tv_sec, data->timer.tv_usec);
 
       struct frame * fr = &data->FT[vp->f];
-      fr->state = FR_DIRTY;	//we have written to frame, mark it dirty
+      fr->state = FR_DIRTY;	//we have written to frame, marking it as dirty
 
       reply = 1;
     }else if(usr->ref.state == REF_DENY){
@@ -667,7 +670,7 @@ static int schedule_dispatch(){
 
     mstat.num_req++;
 
-    mbuf.mtype = usr->pid;  //set its pid as type
+    mbuf.mtype = usr->pid;  //setting its pid as type
     mbuf.user_id = usr->id;
     if(msgsnd(qid, (void*)&mbuf, MSGBUF_SIZE, 0) == -1){
   		perror("msgrcv");
@@ -675,7 +678,7 @@ static int schedule_dispatch(){
   	}
   }
 
-  //calculate dispatch time
+  //calculating the dispatch time
   tv.tv_sec = 0; tv.tv_usec = rand() % 100;
   timerinc(&data->timer, &tv);
   printf("OSS: total time this dispatching was %li nanoseconds at system time %lu:%li\n", tv.tv_usec, data->timer.tv_sec, data->timer.tv_usec);
@@ -690,7 +693,7 @@ static void print_memmap(){
   printf("Currently available memory at time %li.%li\n", data->timer.tv_sec, data->timer.tv_usec);
 	mstat.num_lines++;
 
-  printf("\t\t\t\tOccupied\tDirtyBit\ttimeStamp\n");
+  printf("\t\t\tOccupied\tDirtyBit\ttimeStamp\n");
   for(i=0; i < FTBL_SIZE; i++){
     const struct frame * fr = &data->FT[i];
     if(fr->p >= 0){
@@ -713,11 +716,11 @@ int main(const int argc, char * const argv[]){
       clean_exit(EXIT_FAILURE);
   }
 
-  //our clock step of 100 ns
+  //this is the clock step of 100 ns
   timestep.tv_sec = 0;
   timestep.tv_usec = 100;
 
-  //ignore signals to avoid interrupts in msgrcv
+  //ignoring the signals to avoid interrupts in msgrcv
   signal(SIGINT,  sig_handler);
   signal(SIGTERM, sig_handler);
   signal(SIGCHLD, sig_handler);
@@ -727,23 +730,23 @@ int main(const int argc, char * const argv[]){
 
   bzero(&mstat, sizeof(struct mstat));
 
-  q_init(&blocked, started.max);  //initialize the queue
-  //allocate and setup memory
+  q_init(&blocked, started.max);  //initializing the queue
+  //allocating and setting memory
   mem_alloc();
 
 	while(loop_flag){
 
-    //clock moves forward
+   //clock keeps on moving forward
     timerinc(&data->timer, &timestep);
     if(timercmp(&data->timer, &timer_fork, >=) != 0){
 
       if(started.val < started.max){
         spawn_user();
-      }else{  //we have generated all of the children
+      }else{  //if we have generated all of the children at this stage
         break;
       }
 
-      //set next fork time
+      //setting the next fork time
       timer_fork.tv_sec  = data->timer.tv_sec + (rand() % maxTimeBetweenNewProcsSecs);
       timer_fork.tv_usec = data->timer.tv_usec + (rand() % maxTimeBetweenNewProcsNS);
     }
@@ -757,14 +760,15 @@ int main(const int argc, char * const argv[]){
     }
 	}
 
+	//For printing the statistics at the end
   printf("Time taken: %lu:%li\n", data->timer.tv_sec, data->timer.tv_usec);
 
   printf("Number of references: %d\n", mstat.num_req);
-	printf("Number of reads: %d\n", mstat.num_rd);
-	printf("Number of writes: %d\n", mstat.num_wr);
+	printf("Total Number of reads: %d\n", mstat.num_rd);
+	printf("Total Number of writes: %d\n", mstat.num_wr);
 
-	printf("Number of evictions: %d\n", mstat.num_evict);
-	printf("Number of faults: %d\n", mstat.num_fault);
+	printf("Total Number of evictions: %d\n", mstat.num_evict);
+	printf("Total Number of faults: %d\n", mstat.num_fault);
 
 	printf("Memory access / second: %.2f\n", (float) mstat.num_req / data->timer.tv_sec);
 	printf("Faults / reference: %.2f\n", (float)mstat.num_fault / mstat.num_req);
