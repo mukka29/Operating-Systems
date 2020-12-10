189be78915c397bd04741e8028d0c32ce0faa76c
Bharath Muka <mukkadev@gmail.com>
2020-12-10 02:02:01 -0600

    Update user.c

--- Assignment6/user.c
+++ Assignment6/user.c
@@ -1,3 +1,4 @@
+//header files
 #include <string.h>
 #include <sys/shm.h>
 #include <sys/msg.h>
@@ -12,8 +13,8 @@
 
 #include "data.h"
 
-static int mid=-1, qid = -1, sid = -1;	//memory and msg queue identifiers
-static struct data *data = NULL;	//shared memory pointer
+static int mid=-1, qid = -1, sid = -1;	//identifiers for memory and message queue
+static struct data *data = NULL; //creating pointer for shared memory
 static struct user_pcb * user = NULL;
 static unsigned int user_id = -1;
 static unsigned int user_scheme = 0;	//-m option at master for memory address generation
@@ -26,7 +27,7 @@ static void update_weights(){
 	}
 }
 
-//Attach the shared memory pointer
+//Attaching the shared memory pointer
 static int attach_data(const int argc, char * const argv[]){
 	int n;
 	key_t k1 = ftok(PATH_KEY, MEM_KEY);
@@ -37,28 +38,28 @@ static int attach_data(const int argc, char * const argv[]){
 		return -1;
 	}
 
-	//get shared memory id
+	//to get shared memory id
 	mid = shmget(k1, 0, 0);
   if(mid == -1){
   	perror("shmget");
   	return -1;
   }
 
-	//get message queue ud
+	//to get message queue ud
 	qid = msgget(k2, 0);
   if(qid == -1){
   	perror("msgget");
   	return -1;
   }
 
-	//get shared memory id
+	//to get shared memory id
 	sid = semget(k3, 0, 0);
   if(sid == -1){
   	perror("semget");
   	return -1;
   }
 
-	//attach to the shared memory
+	//attaching to the shared memory
 	data = (struct data *) shmat(mid, NULL, 0);
 	if(data == NULL){
 		perror("shmat");
@@ -89,7 +90,7 @@ static int critical_lock(){
 	return 0;
 }
 
-//Unlock critical section
+//Unlocking the critical section
 static int critical_unlock(){
 	struct sembuf sop;
 	sop.sem_num = 0;
@@ -142,32 +143,32 @@ static int execute(){
 		//decide between MEM_READ(0), MEM_WRITE(1)
 		decision = (r < READ_PROBABILITY) ? MEM_READ /*MEM_READ*/ : MEM_WRITE /* MEM_WRITE */;
 	//}else{
-	//	return 1;	//stop master loop
+	//	return 1;	//stop the master loop
 	//}
 
-	//setup the reference
+	//setting up the reference
 	critical_lock();
 	user->ref.req = decision;
 	user->ref.vaddr = (user_scheme == 0) ? gen_rand_addr() : gen_weighted_addr();
 	user->ref.state = REF_WAIT;
 	critical_unlock();
 
-	//send our request
+	//sending our request
 	mbuf.mtype = getppid();
 	mbuf.user_id = user_id;
 	if(msgsnd(qid, &mbuf, MSGBUF_SIZE, 0) == -1){
 		perror("msgsnd");
-		return 1;	//stop master loop
+		return 1;	//stop the master loop
 	}
 
-	//wait for reply from oss
+	//waiting for a reply from oss
 	if(msgrcv(qid, (void*)&mbuf, MSGBUF_SIZE, getpid(), 0) == -1){
 		perror("msgrcv");
-		return 1;	//stop master loop
+		return 1;	//stop the master loop
 	}
 
 	if(mbuf.user_id == -1){
-		return 1;	//stop master loop
+		return 1;	//stop the master loop
 	}
 
 	return 0;
@@ -183,7 +184,7 @@ static int execute_final(){
 	user->ref.state = REF_WAIT;
 	critical_unlock();
 
-	//send our request
+	//sending our request
 	mbuf.mtype = getppid();
 	mbuf.user_id = user_id;
 	if(msgsnd(qid, &mbuf, MSGBUF_SIZE, 0) == -1){
@@ -205,13 +206,13 @@ int main(const int argc, char * const argv[]){
 	int stop = 0;
 	while(!stop){
 
-		//check for stop on every 1000 references
+		//checking for stop on every 1000 references
 		if(++ref_count > 1000){
 			if(critical_lock() < 0){
 				break;
 			}
 
-			if(data->stop == 1){	//if master wants us to stop
+			if(data->stop == 1){	//in case if master wants us to stop
 				stop = 1;
 				critical_unlock();
 				break;
